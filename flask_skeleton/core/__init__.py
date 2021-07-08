99018c551567995c89f2924d212a82a6e3a47ec5
Mario Hernandez <mariofix@pm.me>
2021-07-08 18:46:40 -0400

    Django Base

--- flask_skeleton/core/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-blueprint = Blueprint(
-    "core",
-    __name__,
-    url_prefix="",
-    template_folder="templates",
-    static_folder="static"
-)
--- flask_skeleton/core/routes.py
+++
@@ -1,6 +0,0 @@
-from flask_skeleton.core import blueprint
-
-
-@blueprint.route("/")
-def route_default():
-    return "/"
